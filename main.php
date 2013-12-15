5803dd926d4107f8c3f0bb6cc8b6d900be833697
Rudy Cortes <fxjeane@gmail.com>
2013-12-15 10:59:05 -0800

    initial commit

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+/*****************************
+ * Plugin Name: RCM User Tags
+ * Description: Plugin to create custom taxonomies from the GUI
+ * Version: 0.1
+ * Author: Rudy Cortes &
+ * Author URI: http://rudycortes.com
+ *****************************/
+

f291812448c054c8b93247135c42d63a84b5e387
Umesh Kumar <umeshsingla05@gmail.com>
2013-12-29 21:20:47 +0530

    template

--- templates/user-taxonomy-template.php
+++ templates/user-taxonomy-template.php
@@ -8,7 +8,11 @@ get_header(); ?>
     <section id="primary" class="content-area">
             <div id="content" class="site-content" role="main">
                 <header class="page-header">
-                    <h1 class="page-title"> <?php _e( get_query_var('term').':', UT_TRANSLATION_DOMAIN); ?> </h1>
+                    <?php $taxonomy = get_taxonomy(get_query_var('taxonomy')); 
+                    $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); ?>
+                    <h1 class="page-title"> <?php 
+                        echo apply_filters( 'ut_template_heading', sprintf( '%s: %s', __( $taxonomy->labels->name, UT_TRANSLATION_DOMAIN ), __( $term->name , UT_TRANSLATION_DOMAIN) ), $taxonomy, $term ); 
+                    ?> </h1>
                 </header> <?php
                 $term_id = get_queried_object_id();
                 $term = get_queried_object();
@@ -16,15 +20,17 @@ get_header(); ?>
                 $users = get_objects_in_term( $term_id, $term->taxonomy );
 
                 if (!empty($users)) {
-                    foreach ($users as $user_id) { ?>
-                        <div class="user-entry">
-                            <?php echo get_avatar(get_the_author_meta('email', $user_id), '96'); ?>
-                            <h2 class="user-title"><a href="<?php echo esc_url(get_author_posts_url($user_id)); ?>"><?php the_author_meta('display_name', $user_id); ?></a></h2>
-                            <div class="description">
-                                <?php echo wpautop(get_the_author_meta('description', $user_id)); ?>
+                    foreach ($users as $user_id) { 
+                        $template_content = '
+                        <div class="user-entry">'.
+                            get_avatar(get_the_author_meta('email', $user_id), '96').'
+                            <h2 class="user-title"><a href="' .esc_url(get_author_posts_url($user_id)).'">'. get_the_author_meta('display_name', $user_id).'</a></h2>
+                            <div class="description">'.
+                                wpautop(get_the_author_meta('description', $user_id)).'
                             </div>
-                        </div>
-                    <?php } ?>
+                        </div>';
+                     }
+                echo apply_filters( 'ut_tepmplate_content', $template_content, $users, $taxonomy, $term ); ?>
                 <?php } ?>
             </div><!-- #content -->
     </section><!-- #primary --> <?php
